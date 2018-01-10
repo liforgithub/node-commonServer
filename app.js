a360ffadffb251300b92b35c037cbfc84cdb20a3
李雪洋 <lixueyang@qianmi.com>
2018-01-10 17:02:04 +0800

    koa微框架

---
+++ app.js
@@ -0,0 +1,71 @@
+const Koa = require('koa');
+const app = new Koa();
+const router = require('koa-router')();
+const views = require('koa-views');
+const co = require('co');
+const convert = require('koa-convert');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser')();
+const logger = require('koa-logger');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+const api = require('./routes/api');
+//log工具
+const logUtil = require('./utils/log_util');
+
+const response_formatter = require('./middlewares/response_formatter');
+
+// middlewares
+app.use(convert(bodyparser));
+app.use(convert(json()));
+app.use(convert(logger()));
+app.use(require('koa-static')(__dirname + '/public'));
+
+
+
+app.use(views(__dirname + '/views', {
+  extension: 'jade'
+}));
+
+// logger
+app.use(async (ctx, next) => {
+  //响应开始时间
+  const start = new Date();
+  //响应间隔时间
+  var ms;
+  try {
+    //开始进入到下一个中间件
+    await next();
+
+    ms = new Date() - start;
+    //记录响应日志
+    logUtil.logResponse(ctx, ms);
+
+  } catch (error) {
+    
+    ms = new Date() - start;
+    //记录异常日志
+    logUtil.logError(ctx, error, ms);
+  }
+
+});
+
+//添加格式化处理响应结果的中间件，在添加路由之前调用
+//仅对/api开头的url进行格式化处理
+app.use(response_formatter('^/api'));
+
+router.use('/', index.routes(), index.allowedMethods());
+router.use('/users', users.routes(), users.allowedMethods());
+router.use('/api', api.routes(), api.allowedMethods());
+
+app.use(router.routes(), router.allowedMethods());
+// response
+app.on('error', function(err, ctx){
+  console.log(err)
+  logger.error('server error', err, ctx);
+});
+
+
+module.exports = app;
