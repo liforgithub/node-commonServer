c0febc231c65615081b4e18ec9c004bba6f98a4a
李雪洋 <lixueyang@qianmi.com>
2018-01-12 13:44:54 +0800

    book查询接口新增分页

--- app/controllers/bookcontroller.ts
+++ app/controllers/bookcontroller.ts
@@ -3,9 +3,9 @@ import tbBook from '../dao/tbBook';
 export default class bookController {
 
     static queryBookList = async (ctx, next) => {
-        let count = await tbBook.count();
+        let count = await tbBook.count(ctx);
         count = count[0]['count(1)'];
-        let list = await tbBook.query() || [];
+        let list = await tbBook.query(ctx) || [];
         ctx.body = {
             dataList: list,
             totalCount: count
--- app/dao/tbBook.ts
+++ app/dao/tbBook.ts
@@ -2,11 +2,13 @@ import baseDao from "./baseDao";
 
 export default class tbBook extends baseDao {
 
-    static query() {
+    static query(ctx) {
         let sqlCon = this.getSqlCon();
         return new Promise((resolve, reject) => {
             sqlCon.getConnection((err, conn) => {
                 let sql = "select * from tb_book where 1=1";
+
+                sql += " LIMIT " + ctx.query.pageNum * ctx.query.pageSize + "," + ctx.query.pageSize;
                 conn.query(sql, (err, result) => {
                     conn.release();
                     resolve(result);
@@ -15,7 +17,7 @@ export default class tbBook extends baseDao {
         })
     }
 
-    static count() {
+    static count(ctx) {
         let sqlCon = this.getSqlCon();
         return new Promise((resolve, reject) => {
             sqlCon.getConnection((err, conn) => {
